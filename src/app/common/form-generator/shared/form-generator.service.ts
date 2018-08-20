3b8a42f9baf7b3a211138eb2d57d15d4505cd4e1
Malin Boyanov <mboyanov@develop-soft.com>
2018-08-20 17:59:16 +0300

    added

--- src/app/common/form-generator/shared/form-generator.service.ts
+++ src/app/common/form-generator/shared/form-generator.service.ts
@@ -1,6 +1,7 @@
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
 import { map } from 'rxjs/operators';
+import { Factory } from 'src/app/common/form-generator/shared/factory/factory';
 
 @Injectable({
   providedIn: 'root'
@@ -11,13 +12,19 @@ export class FormGeneratorService {
 
   public build(schema$: Observable<any>) {
     return schema$.pipe(
-      map((data: any) => {
-        
+      map((data: Array<any>) => {
+        const group = this.createGroup(data);
         return {
           formModel: [],
           formGroup: {}
         }
       })
     )
+  }
+
+  private createGroup(data) {
+    return data.map((element: any) => {
+      return Factory.createField(element);
+    })
   }
 }
